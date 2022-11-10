257294d26a11235a818079323dc3a11f07d5c5b9
MilanGrus <114644504+MilanGrus@users.noreply.github.com>
2022-11-10 12:59:02 +0100

    hotfix

--- zadaniePHP.php
+++ zadaniePHP.php
@@ -1,22 +1,24 @@
-<style type=”text/css”> //Vizual
+
+<?php
+
+header("Content-type: text/css");
+
+$font_family = 'Arial, Helvetica, sans-serif';
+$font_size = '0.7em';
+$border = '1px solid';
+
+echo "<table>";
+echo '<td>hello</td>'
+
+?>
 
 table {
     margin: 8px;
 }
 
 td {
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: .7em;
+    font-family: <?=$font_family?>;
+    font-size: <?=$font_size?>;
     color: #FF0000;
-    border: 1px solid #DDD;
+    border: <?=$border?> #000;
 }
-
-</style>
-
-
-<?php
-
-echo "<table>";
-echo '<td>hello</td>'
-
-?>
