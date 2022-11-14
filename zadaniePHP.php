4a4d5d36c02d164cde5f2520d47391a07b41cb8a
MilanGrus <114644504+MilanGrus@users.noreply.github.com>
2022-11-14 11:27:55 +0100

    ll

--- zadaniePHP.php
+++ zadaniePHP.php
@@ -5,10 +5,11 @@ header("Content-type: text/css");
 
 $font_family = 'Arial, Helvetica, sans-serif';
 $font_size = '0.7em';
+$font_colour = 'red';
 $border = '1px solid';
 
-echo "<table>";
-echo '<td>hello</td>'
+//echo "<table>";
+echo "<p>hello</p>";
 
 ?>
 
