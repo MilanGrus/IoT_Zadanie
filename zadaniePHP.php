46f9a0f1b1c907f535e037cc78e6df37402e794f
MilanGrus <114644504+MilanGrus@users.noreply.github.com>
2022-11-17 19:41:11 +0100

    Add files via upload

--- zadaniePHP.php
+++ zadaniePHP.php
@@ -12,7 +12,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS.css" type="text/css">
-    <link rel="shortcut icon" href="https://spng.pngfind.com/pngs/s/177-1775074_bonfire-illustration-hd-png-download.png">
+    <link rel="shortcut icon" href="image-bonfire-icon.png" type="image/icon type">
     <title>IoT Zadanie 2</title>
 </head>
 
