bc714ddf69cbd74f04438cf44dc82a46c9ad3fba
MilanGrus <114644504+MilanGrus@users.noreply.github.com>
2022-11-17 18:13:02 +0100

    Add files via upload

--- zadaniePHP.php
+++ zadaniePHP.php
@@ -1,9 +1,42 @@
-<?php>
-$sn1 = $_GET["a"];
-$sn2 = $_GET["b"];
+<?php
+    $file1 = fopen("output.txt","w") or die("Unable to open file!");
 
-echo '<h1>table</h1>';
-
-$text = "a=" . $sn1 . " b=" . $sn2;
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS.css" type="text/css">
+    <link rel="ikona" href="ikona.png" type="image/icon type">
+    <title>Document</title>
+</head>
+
+<body>
+
+<header>
+   
+   <h1 class="b">Dotazník</h1>
+      
+</header>
+
+<article>
+      
+      <h2>Svet</h2>
+      <p>Na počátku v nicotě bylo pradávné vejce. Z něj se zrodila Asha (Dračice řádu) a její bratr Urgash (Drak chaosu)… Asha dala vzniknout světu Ashanu, ale Urgash jej chtěl zničit. Ještě před první pradávnou válkou dala Asha život 6 elementárním drakům. Poté vypukla válka. Asha v ní zvítězila a uzavřela Urgashe do jádra Ashanu. Bohužel sama byla příliš vysílená a tak ulehla na jeden ze tří měsíců obíhajících Ashan k odpočinku (3 měsíce souvisí se 3 tvářemi Ashy a taky se třemi tvářemi Matky Namtaru… jsou to panna, stařena a věštkyně – život, osud, smrt).</p>
+      <h2> Dejiny</h2>
+      <p>Heroes 5 je příběhem Války královny Isabely. Při krvavém zatmění číslo 5 před 20 lety došlo k ocejchování dítěte (za pomocí Agraela). Tím dítětem byla Isabela z Chrtího vévodství. Šesté zatmění přišlo velice brzy po tom pátém a velice nečekanně (slepí bratři jej nepředvídali). Démoni byly nuceni přerušit svatbu, protože Isabela musela zůstat neposkvrněná. Důvod, proč démonická vojska nebyla tak silná, bylo, že jim nešlo o ničení, ale jen o zajmutí Isabely. Abatyše Beatrice (ve skutečnosti Biara) fungovala jako špeh. Zajmutí se podařilo Agraelovi, ale Godrik z rodu Jednorožců ji vysvobodil. Král Alaron (elf) poslal sice legie válečníků ale ty byly zmasakrovány. Cyrus (mág) pomoc odmítá. Ke konci války je zabit Isabelin nastávající Nikolai a Isabela slibuje pomstu. Sama se ujímá trůnu, to způsobuje problémy, protože by měl nastoupit Nikolaiův příbuzný Andrei (všiměte si ruských jmen Gryfí dynastie).</p>
+      
+      
+      
+      
+      
+</article>
+
+</body>
+
+</html>
